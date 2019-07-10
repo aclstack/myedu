e48b4751050c7dd77de5912031bb32e9a00acacc
aclstack <aclstack@gmail.com>
2019-07-10 19:30:42 +0800

    second commit

--- apps/message_form/models.py
+++ apps/message_form/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Message(models.Model):
+    # verbose_name 表注释, primary_key设置主键
+    name = models.CharField(max_length=20, verbose_name="姓名", primary_key=True)
+    email = models.EmailField(verbose_name="邮箱")
+    address = models.CharField(max_length=128, verbose_name="联系地址")
+    message = models.TextField(verbose_name="留言信息")
+
+    class Meta:
+        verbose_name = "留言信息"
+        # 如果不设置verbose_name_plural，后台系统会显示为 verbose_name + s
+        verbose_name_plural = "verbose_name"
+        # 指定表名称，默认为Class名
+        db_table = "message"
--- apps/message_form/views.py
+++ apps/message_form/views.py
@@ -1,7 +1,49 @@
 from django.shortcuts import render
-
+from apps.message_form.models import Message
 
 # Create your views here.
 
 def message_form(request):
-    return render(request, 'message_form.html')
+    # 拼凑SQL语句
+    # all_message = Message.objects.all()
+
+    # # 真正执行SQL语句
+    # for message in all_message:
+    #      print(message.name)
+
+    # filter 语句，查不到返回空
+    # all_message = Message.objects.filter(name="bobby")
+
+    # get 返回的是一个对象，数据不存在货多个则会报错,此操作会立即执行，而不再拼凑SQL
+    # message = Message.objects.get(name="bobby1")
+    # 删除操作
+    # message.delete()
+
+    # 数据插入,如果存在则覆盖
+    # message = Message()
+    # message.name ='ZS'
+    # message.email = 'zs@qq.com'
+    # message.address ='北京'
+    # message.message = '咱也不知道，咱也不敢问'
+    # message.save()
+
+    # 请求提交时，request为uwsgi对象
+    var_dict = {}
+    if request.method == "POST":
+        name = request.POST.get("name", "")
+        email = request.POST.get("email", "")
+        address = request.POST.get("address", "")
+        message_text = request.POST.get("message", "")
+
+        message = Message()
+        message.name = name
+        message.email = email
+        message.address = address
+        message.message = message_text
+        message.save()
+    elif request.method == "GET":
+        all_message = Message.objects.filter()
+        if all_message:
+            message = all_message[0]
+            var_dict = {"message": message}
+    return render(request, 'message_form.html', var_dict)
